6aa9555143aeae9f57ebea35324255d23bbcaf1f
Russ <72242656+Russ741@users.noreply.github.com>
2023-10-03 17:05:30 -0400

    Convert video 2 part b into percent-based worksheet.

--- 02-building-makemore/02b-building_makemore_slp-solutions.py
+++ 02-building-makemore/02b-building_makemore_slp-solutions.py
@@ -1,6 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+
 def load_words():
     words = open("../names.txt").read().splitlines()
     return words
+
+# %%
 
 def generate_bigrams(words):
     bigrams = []
@@ -11,6 +30,8 @@ def generate_bigrams(words):
         bigrams.append((word[-1], '.'))
     return bigrams
 
+# %%
+
 def get_stoi(bigrams):
     chars = set()
     for bigram in bigrams:
@@ -19,15 +40,21 @@ def get_stoi(bigrams):
     stoi = { v:k for (k, v) in enumerate(sorted(chars))}
     return stoi
 
+# %%
+
 def get_itos(stoi):
     itos = {stoi[c]:c for c in stoi}
     return itos
+
+# %%
 
 def get_x_and_y(bigrams, stoi):
     x = torch.tensor(list(map(lambda bigram : stoi[bigram[0]], bigrams)))
     y = torch.tensor(list(map(lambda bigram : stoi[bigram[-1]], bigrams)))
 
     return x, y
+
+# %%
 
 import torch
 def initialize_w_b(stoi):
@@ -36,6 +63,8 @@ def initialize_w_b(stoi):
     b = torch.zeros((1,stoi_n),dtype=torch.float64, requires_grad=True)
 
     return W, b
+
+# %%
 
 def forward_prop(x, W, b):
     one_hot = torch.nn.functional.one_hot(x).double()
@@ -46,15 +75,21 @@ def forward_prop(x, W, b):
 
     return softmax
 
+# %%
+
 def calculate_loss(y_hat, y):
     match_probabilities = y_hat[torch.arange(len(y)), y]
     neg_log_likelihood = -match_probabilities.log().mean()
     return neg_log_likelihood
 
+# %%
+
 def descend_gradient(W, b, learning_rate):
     W.data -= learning_rate * W.grad
     b.data -= learning_rate * b.grad
     return W, b
+
+# %%
 
 def main():
     words = load_words()
