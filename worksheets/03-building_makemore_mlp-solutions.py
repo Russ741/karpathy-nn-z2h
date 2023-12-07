5084ef96943557062dcc8de4e9dbf6a28bab763a
Russ <72242656+Russ741@users.noreply.github.com>
2023-12-06 21:38:53 -0500

    W3: Add requires_grad video link for get_C and  initialize_W_b.

--- worksheets/03-building_makemore_mlp-solutions.py
+++ worksheets/03-building_makemore_mlp-solutions.py
@@ -265,7 +265,7 @@ test_get_X_and_Y()
 # And returns:
 # * a ```torch.Tensor``` of ```float64``` (```C```) representing the initial (random) embedding vectors for each index.
 #
-# Video: [0:03:01](https://youtu.be/TCH_1BHY58I?t=181) and [0:12:19](https://youtu.be/TCH_1BHY58I?t=739)
+# Video: [0:03:01](https://youtu.be/TCH_1BHY58I?t=181), [0:12:19](https://youtu.be/TCH_1BHY58I?t=739), and [0:38:49](https://youtu.be/TCH_1BHY58I?t=2329)
 
 # %%
 import torch
@@ -362,7 +362,7 @@ test_get_vector_embedding()
 # * a one-dimensional pytorch.Tensor ```b``` of length ```neuron_ct```
 #   * the elements of ```b``` can be zero
 #
-# Video: [0:18:37](https://youtu.be/TCH_1BHY58I?t=1117) and [0:29:17](https://youtu.be/TCH_1BHY58I?t=1757)
+# Video: [0:18:37](https://youtu.be/TCH_1BHY58I?t=1117), [0:29:17](https://youtu.be/TCH_1BHY58I?t=1757), and [0:38:49](https://youtu.be/TCH_1BHY58I?t=2329)
 
 # %%
 import torch
